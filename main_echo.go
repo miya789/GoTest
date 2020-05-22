ccd5b5ae9cb32d62c8ebaa3fe38e9609047c5ce6
miya789 <miya.nori42395@gmail.com>
2020-05-22 18:11:45 +0900

    Add test file for echo

---
+++ main_echo.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// e.Use(middleware.Logger())
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":1234"))
+	// fmt.Printf("start listening server at %s\n", "8080")
+	// e.Start(fmt.Sprintf(":%v", 8080))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello world!")
+}
